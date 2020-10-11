078ed10284f1db379611842500694644bfd51b4d
Sebastian Bigos <sebastianbigos@yahoo.com>
2020-10-11 16:45:44 -0500

    JavaScript code for My Dice Roller

---
+++ dice-roller-sb.js
@@ -0,0 +1,26 @@
+var dice = {
+    sides: 6,
+    roll: function () {
+      var randomNumber = Math.floor(Math.random() * this.sides) + 1;
+      return randomNumber;
+    }
+  }
+  
+  
+  
+  //Prints dice roll to the page
+  
+  function printNumber(number) {
+    var placeholder = document.getElementById('placeholder');
+    placeholder.innerHTML = number;
+  }
+  
+  var button = document.getElementById('button');
+  
+  button.onclick = function() {
+    var result = dice.roll();
+    printNumber(result);
+    if (result == 6) {
+        alert("You won!");
+    }
+  }
